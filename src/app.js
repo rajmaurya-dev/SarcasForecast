30c2cfb0d6ae07b245d9459f18150e3807b8a81a
Raj <blcc.raj@gmail.com>
2023-04-02 13:27:55 +0530

    basic changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 //! Public path
 const publicStaticPath = path.join(__dirname, "../public");
